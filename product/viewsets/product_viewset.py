6634aed5f6da56b554c1e369262f4164ef7c389e
Eduardo <eduardoantune12@gmail.com>
2025-11-07 14:01:14 -0300

    (fix)test: changed authentication from product to order

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,5 +1,4 @@
-from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -7,8 +6,7 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [BasicAuthentication, SessionAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
